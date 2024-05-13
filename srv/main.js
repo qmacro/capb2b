9f6f4ffc7d1c900cbf929ca4cde77a5891479e3a
DJ Adams <qmacro@gmail.com>
2024-05-13 09:58:51 +0000

    further explorations into basic 'on' handlers

--- srv/main.js
+++ srv/main.js
@@ -3,7 +3,12 @@ console.log("Hello, World!")
 const cds = require('@sap/cds')
 module.exports = cds.service.impl(function () {
     console.log("I am in the anonymous function")
-    this.on('READ', 'Books', () => {
-        console.log("Handling READ of Books")
+    this.on('READ', 'Books', (req, ...otherstuff) => {
+        return [{
+            ID: cds.utils.uuid(),
+            title: "The title",
+            author_ID: 2
+        }]
+
     })
 })
